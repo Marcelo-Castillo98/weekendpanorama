9d374744e0ac2dad5d63fd2fe7735c8fe57e2df6
CamilaNikole <112881497+CamilaNikole@users.noreply.github.com>
2022-10-14 10:29:56 -0400

    Prueba de Local Storage

--- demo/front-end/javaScript/javaslogin.js
+++ demo/front-end/javaScript/javaslogin.js
@@ -55,17 +55,66 @@ function envioDatoL() {
             }
             else if(respuestal[3]==true){
                 localStorage.setItem('isLoginUser',respuestal[3]);
-                location.href = "main.html"
+
+                localStorage.setItem('currenUserData',JSON.stringify(data));
+        
+                window.location.assign("main.html");
+                
             }
             else{
                 swal("Ha ocurrido un error.", "error al acceder a la base de datos", "error");
             }
         });
+
+        function verifyIsLogin(pageIsLoginRedirect = 'false',currentPage='default'){
+	
+            let isLoginUser = localStorage.getItem('isLoginUser');
+            console.log(pageIsLoginRedirect,isLoginUser,currentPage,isLoginUser == 'true');
+            
+            if(isLoginUser == 'true'){
+                console.log(pageIsLoginRedirect,isLoginUser,currentPage);
+                if(pageIsLoginRedirect == 'true'){
+                    window.location.assign("main.html"); 
+                }
+                
+            }else{
+                if(currentPage != 'Login'){
+                    window.location.assign("Login.html");
+                }        
+            }
+            
+        }
+
+        function logoutUser(){
+	
+            let isLoginUser = localStorage.getItem('isLoginUser');
+            
+            if(isLoginUser){
+                localStorage.removeItem('isLoginUser');
+                localStorage.removeItem('currenUserData');
+            }
+            window.location.assign("Login.html")   
+            
+        }
+        
+        function getDataUser(){
+            let dataUser = JSON.parse(localStorage.getItem('currenUserData'));
+            return dataUser;
+        }
+        
+        function welcomeName(){
+            let name = document.getElementById('welcomeUser');    
+            name.innerText = getDataUser().nombre;
+        }
         //console.log(respuestal)
         //.then(response => response.json())
         //.then(hola => response)
             //console.log(response)
-            
+       
+
+
+
+
     }
 
     
